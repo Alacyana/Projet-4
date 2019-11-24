d95349f0360171be0db9c06795217bf673f78ed2
Biabaud Lila <lila.biabaud@gmail.com>
2019-11-24 16:12:16 +0100

    Ajout fonction dates

--- model/Manager.php
+++ model/Manager.php
@@ -2,13 +2,17 @@
 
 namespace Projet\Model;
 
-require("Projet-4/config.php");
-
 class Manager
 {
 	protected function dbConnect()
 	{
-		$request = new PDO('mysql:host=DB_HOST;dbname=DB_NAME;charset=utf8','DB_USERNAME','DB_MDP');
+		$request = new \PDO('mysql:host='. DB_HOST .';dbname='. DB_NAME .';charset=utf8', DB_USERNAME, DB_MDP);
 		return $request;
+	}
+	public function datesZone()
+	{
+		date_default_timezone_set('Europe/Paris');
+		$dates= date("Y-m-d H:i:s");
+		return $dates;
 	}
 }
